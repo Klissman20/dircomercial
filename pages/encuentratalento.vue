b04678c7254b03dd4edbd371af04a7a22011889c
David Roman <roman.david@gmail.com>
2023-10-18 01:31:04 -0500

    encuentrataliento

---
+++ pages/encuentratalento.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="flex justify-center pb-16 pt-4">
+      <div class="mt-4 flex gap-2 m-auto w-3/4 flex-wrap justify-center">
+        <button class="hover:scale-125" @click="setPage(1)">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            class="w-6 h-6 mr-1 stroke-[#707070]"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0z"
+            />
+          </svg>
+        </button>
+        <template v-for="p in pages">
+          <button
+            v-if="page <= p + 3 && page >= p - 3"
+            :key="p"
+            :class="page === p ? 'bg-[#707070] text-white' : ''"
+            class="border border-[#707070] px-2 rounded-lg text-[#707070] focus:scale-125"
+            @click="setPage(p)"
+          >
+            {{ p }}
+          </button>
+        </template>
+        <button class="hover:scale-125" @click="setPage(pages)">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            class="w-6 h-6 ml-1 stroke-[#707070]"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const page = ref(1);
+const pages = ref(1);
+const setPage = (pag: number) => {
+  page.value = pag;
+  //getComercios();
+};
+</script>
