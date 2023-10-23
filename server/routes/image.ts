918ac37e3b352f79127f372de89bb58ce14d208d
David Roman <roman.david@gmail.com>
2023-10-23 00:09:13 -0500

    sendStream h3

--- server/routes/image.ts
+++ server/routes/image.ts
@@ -1,16 +1,17 @@
-import { readFileSync } from "fs";
-import path from "path";
+import { createReadStream } from "node:fs";
+//import path from "path";
+import { sendStream } from "h3";
 export default defineEventHandler(async (event) => {
   const { id } = getQuery(event);
   const url = `https://sbnpljpwdvevewdhzkwv.supabase.co/storage/v1/object/public/images/${id}.jpg`;
-  const file = path.join("data", "./logo-guatape.png");
-  let buffer = readFileSync(file);
+  //const file = path.join("data", "./logo-guatape.png");
+  //let buffer = readFileSync(file);
   try {
     const blob: any = await $fetch(url, {
       method: "GET",
     });
     const arrayBuffer = await blob.arrayBuffer();
-    buffer = Buffer.from(arrayBuffer, "base64");
+    const buffer = Buffer.from(arrayBuffer, "base64");
   } catch (_) {}
-  return buffer;
+  return sendStream(event, createReadStream("./data/logo-guatape.png"));
 });
