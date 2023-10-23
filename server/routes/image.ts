dedfdf7aecfbbf5f41a12f44921ed28444bd8bbc
David Roman <roman.david@gmail.com>
2023-10-22 23:56:12 -0500

    save in data

--- server/routes/image.ts
+++ server/routes/image.ts
@@ -1,14 +1,16 @@
+import { readFileSync } from "fs";
+import path from "path";
 export default defineEventHandler(async (event) => {
   const { id } = getQuery(event);
   const url = `https://sbnpljpwdvevewdhzkwv.supabase.co/storage/v1/object/public/images/${id}.jpg`;
-  const file = await useStorage().getItem("assets/server/logo-guatape.png");
+  const file = path.join("data", "./logo-guatape.png");
+  let buffer = readFileSync(file);
   try {
     const blob: any = await $fetch(url, {
       method: "GET",
     });
     const arrayBuffer = await blob.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer, "base64");
-    return buffer;
+    buffer = Buffer.from(arrayBuffer, "base64");
   } catch (_) {}
-  return file;
+  return buffer;
 });
