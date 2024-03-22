2d5a8ecf56ca30921dd108b8ddb3539e9c896e9b
David Roman <roman.david@gmail.com>
2024-03-22 01:07:03 -0500

    log

--- server/routes/image.ts
+++ server/routes/image.ts
@@ -2,7 +2,7 @@ const runtimeConfig = useRuntimeConfig()
 export default defineEventHandler(async (event) => {
   const { id } = getQuery(event);
   const url = `https://sbnpljpwdvevewdhzkwv.supabase.co/storage/v1/object/public/images/${id}.jpg`;
-  
+  console.log(runtimeConfig.public.baseUrl)
   try {
     const blob: any = await $fetch(url, {
       method: "GET",
