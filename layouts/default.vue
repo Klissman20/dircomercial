bb8b54f2c36ea908feee023d7f5e785b79bdd631
David Roman <roman.david@gmail.com>
2024-03-22 01:22:30 -0500

    add speed-insights

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+</script>
 <template>
   <div>
+    <NuxtLoadingIndicator />
+    <SpeedInsights />
     <NavBar />
     <ClientOnly>
       <IndexSearch></IndexSearch>
