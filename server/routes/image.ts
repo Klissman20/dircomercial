e70debdf893484f597e4eff1c900c1929725b440
David Roman <roman.david@gmail.com>
2024-03-21 21:24:06 -0500

    fix name logo mpio

--- server/routes/image.ts
+++ server/routes/image.ts
@@ -23,7 +23,7 @@ export default defineEventHandler(async (event) => {
   // });
   // const arrayBuffer1 = await logo.arrayBuffer();
   // return Buffer.from(arrayBuffer1, "base64");
-  let logoPath = path.join(process.cwd(), 'static', 'Logotipo.png');
+  let logoPath = path.join(process.cwd(), 'static', 'logompio.png');
   let logo = fs.readFileSync(logoPath)
   return logo;
 });
